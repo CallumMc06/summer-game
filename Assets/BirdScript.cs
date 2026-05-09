392be7430a39ec119b2e358ce7e2e5450007d519
CallumMc06 <callummcintyre3366@gmail.com>
2026-05-09 18:11:27 -0400

    CloseandReopen

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidBody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            myRigidBody.linearVelocity = Vector2.up * 10;
+        }
     }
 }
