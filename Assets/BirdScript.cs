9ca19a6544e3693ebe5845b7279b1c7688117a94
CallumMc06 <callummcintyre3366@gmail.com>
2026-05-09 18:39:30 -0400

    BirdPhysics

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
+    public float flapStrength;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,7 +15,7 @@ public class BirdScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            myRigidBody.linearVelocity = Vector2.up * 10;
+            myRigidBody.linearVelocity = Vector2.up * flapStrength;
         }
     }
 }
